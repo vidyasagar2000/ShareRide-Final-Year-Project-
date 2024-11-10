bf70162361f94275ea45407575e23606dc32afaf
unknown <vidya9955297836@gmail.com>
2024-11-10 13:39:48 +0530

    Deployed Frontend and Backend

--- shareride/src/socket.jsx
+++ shareride/src/socket.jsx
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
 // https://share-ride-s8da.onrender.com
-// const socket = io('https://share-ride-s8da.onrender.com');
+const socket = io('https://shareride-final-year-project-1.onrender.com');
 export default socket;
