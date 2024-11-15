b5380fc66363d7cdb9223da35b6b36c305edbefa
unknown <vidya9955297836@gmail.com>
2024-11-15 10:57:56 +0530

    Added Sidebar to SLide on Content

--- shareride/src/pages/AppLayout.jsx
+++ shareride/src/pages/AppLayout.jsx
@@ -4,48 +4,56 @@ import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import PopUp from "../components/PopUp";
 import ChatRoom from "../components/ChatRoom";
-// import ChatRoom from "../components/ChatRoom";
-
 
 const AppLayout = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  
+  const [isSidebarOpen] = useState(true);
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
 
-  // const isPopUp=true;
+  const toggleSidebar = () => {
+    setIsMobileSidebarOpen(!isMobileSidebarOpen);
+  };
 
   return (
     <div className="flex flex-col h-screen">
       {/* Header */}
       <div className="w-full">
-        <Header toggleSidebar={setIsSidebarOpen} />
+        <Header toggleSidebar={toggleSidebar} />
       </div>
 
       {/* Content Area */}
-      <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar */}
+      <div className="flex flex-1 overflow-hidden relative">
+        {/* Sidebar for larger screens */}
         {isSidebarOpen && (
-          <div className="w-64"
-          >
-            <Sidebar
-            />
+          <div className="hidden lg:block lg:w-64">
+            <Sidebar />
           </div>
         )}
 
-        {/* Main Content */}
+        {/* Sliding Sidebar overlay for mobile screens */}
+        <div
+          className={`fixed inset-y-0 left-0 z-20 w-64 bg-gray-200 transform transition-transform duration-300 ease-in-out ${
+            isMobileSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } lg:hidden`}
+        >
+          <Sidebar />
+        </div>
 
-       
-       
-        
-        <PopUp/>
-        <main className="flex-1 p-6 bg-gray-100 overflow-y-auto z-0">
+        {/* Overlay for mobile sidebar */}
+        {isMobileSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-black opacity-50 z-10 lg:hidden"
+            onClick={toggleSidebar} // Close sidebar when overlay is clicked
+          ></div>
+        )}
+
+        {/* Main Content */}
+        <main className="flex-1 p-6 bg-gray-100 overflow-y-auto z-0 w-full">
           <Outlet />
         </main>
-        <ChatRoom/>
-      </div>
-      {/* <ChatRoom/> */}
 
-      {/* Footer */}
-      {/* <Footer className="p-4 bg-gray-200 text-center" /> */}
+        <PopUp />
+        <ChatRoom />
+      </div>
     </div>
   );
 };
